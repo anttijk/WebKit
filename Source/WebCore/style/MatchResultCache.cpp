afbd9c78096be73620ec80bcb837afb2be3a00f4
Antti Koivisto <antti@apple.com>
2025-04-17 22:27:05 +0300

    REGRESSION(293802@main): [MacOS Debug] ASSERTION FAILED: WTF::Ref<WTF::Thread, WTF::RawPtrTraits<WTF::Thread>, WTF::DefaultRefDerefTraits<WTF::Thread>>::ptr() const https://bugs.webkit.org/show_bug.cgi?id=291702
    Unreviewed build fix/followup.
    
    * Source/WebCore/style/MatchResultCache.cpp:
    (WebCore::Style::MatchResultCache::resultWithCurrentInlineStyle):
    
    Remove the mistaken (and unnecessary) use of CheckedRef.

--- Source/WebCore/style/MatchResultCache.cpp
+++ Source/WebCore/style/MatchResultCache.cpp
@@ -141,7 +141,7 @@ const std::optional<CachedMatchResult> MatchResultCache::resultWithCurrentInline
     if (it == m_entries.end())
         return { };
 
-    CheckedRef entry = *it->value;
+    auto& entry = *it->value;
 
     auto* styledElement = dynamicDowncast<StyledElement>(element);
     RefPtr inlineStyle = styledElement ? styledElement->inlineStyle() : nullptr;
@@ -154,9 +154,9 @@ const std::optional<CachedMatchResult> MatchResultCache::resultWithCurrentInline
     auto changedProperties = computeAndUpdateChangedProperties(entry);
 
     return CachedMatchResult {
-        .unadjustedStyle = copy(entry->unadjustedStyle),
+        .unadjustedStyle = copy(entry.unadjustedStyle),
         .changedProperties = WTFMove(changedProperties),
-        .styleToUpdate = *entry->unadjustedStyle.style
+        .styleToUpdate = *entry.unadjustedStyle.style
     };
 }
 
